5be0df4b5efff799bafee19eddb5dacefd0088cf
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-12-03 19:21:16 -0500

    Added Page Header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <h3>Welcome to the <span class="highlight">Future</span></h3>
       <div id="title">
         <h1 id="title_header">PineApple Book 11</h1>
         <p>Starting at $645</p>
