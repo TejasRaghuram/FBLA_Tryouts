f8d60a069b4bd3fe34be126da0cfbec45beddfc3
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-12-03 20:17:24 -0500

    Added Page Subtitle

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h3>Welcome to the <span class="highlight">Future</span></h3>
+      <h3>The <span class="highlight">Future</span> is now.</h3>
+      <h4>Stunningly <span class="highlight">Small</span></h4>
       <div id="title">
         <h1 id="title_header">PineApple Book 11</h1>
         <p>Starting at $645</p>
